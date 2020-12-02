4125334c26ff7290563eeff09a0fb149e6387432
Anderson Silva <anderson.silva@onfido.com>
2020-12-02 23:50:02 +0000

    Day 1

---
+++ day01/solution.py
@@ -0,0 +1,25 @@
+import itertools
+import math
+
+magic_number = 2020
+
+def check_sum(some_list, magic_number=magic_number):
+    int_list = [int(i) for i in some_list]
+    addition = sum(int_list)
+    if addition == magic_number:
+        product = math.prod(int_list)
+        print("Numbers were: ", int_list)
+        print("Their product is: ", product)
+
+
+with open("../inputs/day01.txt") as f:
+    items = f.read().splitlines()
+    cool_pairs = []
+    # combinations(range(4), 3) --> 012 013 023 123
+    # Part 1
+    for a, b in itertools.combinations(items, 2):
+        check_sum([a, b])
+
+    # Part 2
+    for a, b, c in itertools.combinations(items, 3):
+        check_sum([a, b, c])
